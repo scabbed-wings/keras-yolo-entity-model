d738de1c5897a27ca58c3aaee6d43892e8a27c40
scabbed-wings <scabbed.wings.demon@gmail.com>
2021-02-13 20:03:03 +0100

    Changing imports

--- yolo.py
+++ yolo.py
@@ -17,7 +17,7 @@ from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
 from yolo3.utils import letterbox_image
 import os
 from keras.utils import multi_gpu_model
-import ibis
+import ibis.ibis
 
 class YOLO(object):
     _defaults = {
