34cf487ee8686d49b09e381edfdc805722e7c497
scabbed-wings <scabbed.wings.demon@gmail.com>
2021-02-15 12:28:41 +0100

    Fixing structure on some functions

--- ibis/relations.py
+++ ibis/relations.py
@@ -26,7 +26,9 @@ def condition_satisfied(obt_relations, id_box, ind_class, out_classes): #Search 
         if ind_class == 1:
             if((elem.ind_inp == id_box or elem.ind_out == id_box) and (out_classes[elem.ind_inp] == 2 or out_classes[elem.ind_out] == 2)):
                 find = True
-            elif((elem.ind_inp == id_box or elem.ind_out == id_box) and (out_classes[elem.ind_inp] == 1 or out_classes[elem.ind_out])):
+                print("Condition satisfied entity ")
+        else:
+            if((elem.ind_inp == id_box or elem.ind_out == id_box) and (out_classes[elem.ind_inp] == 1 or out_classes[elem.ind_out])):
                 counter += 1
     
     if counter >= 2 and counter <= 3:
@@ -44,12 +46,14 @@ def add_solution(obt_relations, new_relation, cont_elem):
             if(elem.ind_box != obt_relations[j].ind_box) and ((elem.ind_inp == obt_relations[j].ind_inp and elem.ind_out == obt_relations[j].ind_out) 
              or (elem.ind_inp == obt_relations[j].ind_out and elem.ind_out == obt_relations[j].ind_inp)):
                 if(new_relation.dist < obt_relations[j].dist and not change_sol):
+                    print("Adding new solution: ", new_relation.ind_box)
                     obt_relations.pop(j)
                     obt_relations.append(new_relation)
                     change_sol = True
             j += 1
     
     if(not change_sol and len(obt_relations) < cont_elem):
+        print("Adding new_solution 2: ", new_relation.ind_box)
         obt_relations.append(new_relation)
     
     return obt_relations
